48ead03175fb2e3d8f66132eabbda1e8387a0aed
kamsi <gruca.kamil@gmail.com>
2014-08-13 17:37:09 +0200

    fixed tests, so that they pass on node 0.8

--- test/index.js
+++ test/index.js
@@ -3,13 +3,13 @@
 var fs = require('fs');
 
 module.exports = function (t, a, d) {
-	fs.readFile('test/__playground/view_example.js', { encoding: 'utf-8' },
+	fs.readFile('test/__playground/view_example.js',
 		function (err, example) {
 			if (err) throw err;
 			fs.readFile('test/__playground/view_example_output.json',
-				{ encoding: 'utf-8' }, function (err2, output) {
+				function (err2, output) {
 					if (err2) throw err2;
-					d(a(JSON.stringify(t(example)), output,
+					d(a(JSON.stringify(t(example)), String(output),
 						'Is source parser returning context correctly?'));
 				});
 		});
