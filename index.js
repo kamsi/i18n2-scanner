ace383d8e0ac2b4f1f96ff26f9189845ba8c3f42
kamsi <gruca.kamil@gmail.com>
2014-08-14 11:30:58 +0200

    clean up, better error log, memoizee cache

--- index.js
+++ index.js
@@ -1,58 +1,77 @@
 'use strict';
 
-var esniffFun = require('esniff/function')
-  , esniffResolveArgs = require('esniff/resolve-arguments')
-  , clearRaw
-  , validValue = require('es5-ext/object/valid-value')
-  , defaultI18nPrefix = 'i18n'
-  , clearOutput
-  , validStringLiteral = require('esniff/valid-string-literal')
-  , stripComments = require('esniff/strip-comments');
+var validValue         = require('es5-ext/object/valid-value')
+  , memoize            = require('memoizee/plain')
+  , esniffFun          = require('esniff/function')
+  , esniffFun_         = esniffFun('_')
+  , esniffResolveArgs  = require('esniff/resolve-arguments')
+  , isStringLiteral    = require('esniff/is-string-literal')
+  , stripComments      = require('esniff/strip-comments')
+  , customError        = require('es5-ext/error/custom')
+  , getESniffFun
+  , resolveStringLiteral
+  , extractArguments
+  , resolveMessage;
 
-clearOutput = function (str) {
-	try {
-		str = new Function("'use strict'; return " + str)();
-	} catch (e) {
-		throw e;
+getESniffFun = memoize(function (prefix) {
+	return esniffFun(prefix + ".bind", { asProperty: true });
+});
+
+resolveStringLiteral = function (stringLiteral, esniffResult) {
+	stringLiteral = stripComments(stringLiteral).trim();
+	if (!isStringLiteral(stringLiteral)) {
+		throw customError("Invalid invocation with param: " +
+				JSON.stringify(stringLiteral) + " for the following params: "
+				+ JSON.stringify(esniffResult),
+			"INVALID_INVOCATION_PARAM_NOT_STRING");
 	}
-	return str;
+	return new Function("'use strict'; return " + stringLiteral)();
 };
 
-clearRaw = function (raw) {
-	var args = esniffResolveArgs(raw);
+extractArguments = function (esniffResult) {
+	var args = esniffResolveArgs(esniffResult.raw);
 	if (!args.length) {
-		throw "Invalid invocation of _ function with params: " + raw;
+		throw customError("Invalid invocation, " +
+				"esniff passed : " + JSON.stringify(esniffResult),
+			"INVALID_INVOCATION_MISSING_PARAM");
 	}
-	args[0] = validStringLiteral(stripComments(args[0]).trim());
-	if (args.length > 2) { //plural
-		args[1] = validStringLiteral(stripComments(args[1]).trim());
-		return 'n\0' + clearOutput(args[0]) + '\0' + clearOutput(args[1]);
-	}
-	return clearOutput(args[0]);
+	return args;
 };
 
-module.exports = function (fileContent/*, options*/) {
-	var context, result = {}, options, i18Prefix, occurrences, clearKey;
-	fileContent = String(validValue(fileContent));
+resolveMessage = function (esniffResult) {
+	var args = extractArguments(esniffResult);
+	args[0] = resolveStringLiteral(args[0], esniffResult);
+	if (args.length > 2) { //plural
+		args[1] = resolveStringLiteral(args[1], esniffResult);
+		return 'n\0' + args[0] + '\0' + args[1];
+	}
+	return args[0];
+};
+
+module.exports = function (source/*, options*/) {
+	var context, result = {}, options, i18Prefix, occurrences, clearKey,
+		args;
+	source = String(validValue(source));
 	options = Object(arguments[1]);
-	i18Prefix = options.i18Prefix || defaultI18nPrefix;
-	context = esniffFun(i18Prefix + ".bind", { asProperty: true })(fileContent);
+	i18Prefix = options.i18Prefix || 'i18n';
+	context = getESniffFun(i18Prefix)(source);
 	if (context[0] && context[0].raw) {
-		result.context = clearRaw(context[0].raw);
+		args = extractArguments(context[0]);
+		result.context = resolveStringLiteral(args[0], context[0]);
 	} else {
-		result.context = '';
+		result.context = 'default';
 	}
 	result.messages = Object.create(null);
 
-	occurrences = esniffFun('_')(fileContent);
+	occurrences = esniffFun_(source);
 	occurrences.forEach(function (o) {
-		clearKey = clearRaw(o.raw);
+		clearKey = resolveMessage(o);
+		delete o.raw;
 		if (result.messages[clearKey]) {
 			result.messages[clearKey].push(o);
 		} else {
 			result.messages[clearKey] = [o];
 		}
-		delete o.raw;
 	});
 
 	return result;
--- test/index.js
+++ test/index.js
@@ -9,8 +9,7 @@ module.exports = function (t, a, d) {
 			fs.readFile('test/__playground/view_example_output.json',
 				function (err2, output) {
 					if (err2) throw err2;
-					d(a(JSON.stringify(t(example)), String(output),
-						'Is source parser returning context correctly?'));
+					d(a.deep(t(example), JSON.parse(String(output))));
 				});
 		});
 };
